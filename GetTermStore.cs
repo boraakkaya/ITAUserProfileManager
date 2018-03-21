d8d531cc29297abe0b92168bc658b8515181e6e3
boraakkaya <akkaya_bora@hotmail.com>
2018-03-21 16:07:12 -0400

    v0.0.0.2

--- GetTermStore.cs
+++ GetTermStore.cs
@@ -35,8 +35,12 @@ namespace ITAUserProfileManager
                 {
                     TaxonomySession taxonomySession = TaxonomySession.GetTaxonomySession(clientContext);
                     //taxonomySession.UpdateCache();
-                    TermStore termStore = taxonomySession.GetDefaultKeywordsTermStore();
-                    clientContext.Load(termStore, store => store.Name, store => store.Groups.Where(g => g.IsSystemGroup == false && g.IsSiteCollectionGroup == false).Include(group => group.Id, group => group.Name, group => group.Description, group => group.IsSiteCollectionGroup, group => group.IsSystemGroup, group => group.TermSets.Include(termSet => termSet.Id, termSet => termSet.Name, termSet => termSet.Description, termSet => termSet.CustomProperties, termSet => termSet.Terms.Include(t => t.Id, t => t.Description, t => t.Name, t => t.IsDeprecated, t => t.Parent, t => t.Labels, t => t.LocalCustomProperties, t => t.IsSourceTerm, t => t.IsRoot, t => t.IsKeyword))));
+                    TermStore termStore = taxonomySession.GetDefaultSiteCollectionTermStore();
+                    clientContext.Load(termStore, store => store.Name, store => store.Groups.Where(g => g.IsSystemGroup == false && g.IsSiteCollectionGroup == false).Include(group => group.Id, group => group.Name, group => group.Description, group => group.IsSiteCollectionGroup, group => group.IsSystemGroup, group => group.TermSets.Include(termSet => termSet.Id, termSet => termSet.Name, termSet => termSet.Description, termSet => termSet.CustomProperties, termSet => termSet.Terms.Include(t => t.Id, t => t.Description, t => t.Name, t => t.IsDeprecated,t => t.Parent, t => t.Labels, t => t.LocalCustomProperties, t => t.IsSourceTerm, t => t.IsRoot, t => t.IsKeyword, t=>t.TermsCount, t=>t.CustomProperties,
+                        t => t.Terms.Include(t1 => t1.Id, t1 => t1.Description, t1 => t1.Name, t1 => t1.IsDeprecated, t1 => t1.Parent, t1 => t1.Labels, t1 => t1.LocalCustomProperties, t1 => t1.IsSourceTerm, t1 => t1.IsRoot, t1 => t1.IsKeyword, t1 => t1.TermsCount, t1 => t1.CustomProperties,
+                        t1 => t1.Terms.Include(t2 => t2.Id, t2 => t2.Description, t2 => t2.Name, t2 => t2.IsDeprecated, t2 => t2.Parent, t2 => t2.Labels, t2 => t2.LocalCustomProperties, t2 => t2.IsSourceTerm, t2 => t2.IsRoot, t2 => t2.IsKeyword, t2 => t2.TermsCount, t2 => t2.CustomProperties,
+                        t2 => t2.Terms.Include(t3 => t3.Id, t3 => t3.Description, t3 => t3.Name, t3 => t3.IsDeprecated, t3 => t3.Parent, t3 => t3.Labels, t3 => t3.LocalCustomProperties, t3 => t3.IsSourceTerm, t3 => t3.IsRoot, t3 => t3.IsKeyword, t3 => t3.TermsCount, t3 => t3.CustomProperties)))
+                    ))));
 
                     clientContext.ExecuteQuery();
                     ITATermStore itaTermStore = new ITATermStore();
@@ -67,6 +71,26 @@ namespace ITAUserProfileManager
                                 ITATerms itaTerm = new ITATerms();
                                 itaTerm.Name = term.Name != null ? term.Name : "";
                                 itaTerm.Id = term.Id !=null ? term.Id : new Guid();
+
+                                if (term.Labels.Count > 0)
+                                {
+                                    itaTerm.Labels = new List<string>();
+                                    foreach (var label in term.Labels)
+                                    {
+                                        itaTerm.Labels.Add(label.Value);
+                                    }
+                                }
+                                if (term.CustomProperties.Count > 0)
+                                {
+                                    itaTerm.CustomProps = new List<ITATermProperty>();
+                                    foreach (var prop in term.CustomProperties)
+                                    {
+                                        itaTerm.CustomProps.Add(new ITATermProperty() { key = prop.Key, value = prop.Value });
+                                    }
+                                }
+
+                                getSubTerms(term,itaTerm,log);
+                                
                                 itaTermSet.Terms.Add(itaTerm);
                             }
                             grp.TermSets.Add(itaTermSet);
@@ -85,5 +109,43 @@ namespace ITAUserProfileManager
                 return req.CreateResponse(HttpStatusCode.OK, "Errorx : "+ ex.Message);
             }
         }
+        public static void getSubTerms(Term parentTerm, ITATerms parentITATerm, TraceWriter log)
+        {
+            if(parentTerm.TermsCount >0)
+            {
+                parentITATerm.Terms = new List<ITATerms>();
+                
+
+                foreach(var term in parentTerm.Terms)
+                {
+                    LabelCollection col = term.Labels;
+                    
+                    ITATerms itaTerm = new ITATerms();
+                    itaTerm.Name = term.Name != null ? term.Name : "";
+                    itaTerm.Id = term.Id != null ? term.Id : new Guid();
+                    
+                    if(term.Labels.Count >0)
+                    {
+                        itaTerm.Labels = new List<string>();
+                        foreach(var label in term.Labels)
+                        {
+                            itaTerm.Labels.Add(label.Value);
+                        }
+                    }
+                    if(term.CustomProperties.Count > 0)
+                    {
+                        itaTerm.CustomProps = new List<ITATermProperty>();
+                        foreach(var prop in term.CustomProperties)
+                        {
+                            itaTerm.CustomProps.Add(new ITATermProperty() { key = prop.Key, value = prop.Value });
+                        }
+                    }
+                    log.Info(parentITATerm.Name + " ----- " + term.Name);
+                    getSubTerms(term,itaTerm,log);
+                    parentITATerm.Terms.Add(itaTerm);
+                }
+            }
+        }
     }
+    
 }
--- ITATermGroup.cs
+++ ITATermGroup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SharePoint.Client.Taxonomy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace ITAUserProfileManager
 {
-     public class ITATermStore
+    public class ITATermStore
     {
-       public List<ITATermGroup> ITATermGroupList { get; set; }
+        public List<ITATermGroup> ITATermGroupList { get; set; }
     }
     public class ITATermGroup
     {
         public Guid Id { get; set; }
         public String Name { get; set; }
-        public List<ITATermSets> TermSets { get; set;}
+        public List<ITATermSets> TermSets { get; set; }
     }
     public class ITATermSets
     {
@@ -26,5 +27,14 @@ namespace ITAUserProfileManager
     {
         public Guid Id { get; set; }
         public String Name { get; set; }
-    }        
+        public List<ITATerms> Terms { get; set; }
+        public List<string> Labels { get; set; }
+        public List<ITATermProperty> CustomProps { get; set; }
+    }
+    public class ITATermProperty
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+    }
+    
 }
